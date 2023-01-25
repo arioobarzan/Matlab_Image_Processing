028af33aab9f8288d1ce3ce6e34f75168347c005
Amir Mohammad Valipour <ruasoftt@gmail.com>
2023-01-25 17:42:50 +0330

    Complete code

---
+++ ChessBoard_8x8.m
@@ -0,0 +1,22 @@
+clc ; clear; close;
+img = zeros(8,8);
+
+for j=1:8
+    modj = mod(j,2);
+    for i=1:8
+        modi = mod(i,2);
+        if modj == 0 
+            if modi == 0
+                img(j,i)=255;
+            end
+        elseif modj ~= 0
+            if modi ~= 0
+                img(j,i)=255;
+            end
+        end
+    end
+end
+
+img = uint8(img);imshow(img);
+
+        
